1ff7b1b1aa362f278421717fc2addadff94ff662
hasnyx <nicavilson12@gmail.com>
2026-02-25 09:34:01 +0200

    Add files via upload

--- Laboratory1/longest_peak.py
+++ Laboratory1/longest_peak.py
@@ -1,11 +1,14 @@
 def longest_peak(array):
     longest_peak_length = 0
+    peak_summit_index = -1 
     i = 1
 
     while i < len(array) - 1:
         is_peak = array[i] > array[i - 1] and array[i] > array[i + 1]
 
         if is_peak:
+            current_summit_idx = i 
+            
             left_idx = i - 2
             while left_idx >= 0 and array[left_idx] < array[left_idx + 1]:
                 left_idx -= 1
@@ -18,9 +21,10 @@ def longest_peak(array):
             
             if current_peak_length > longest_peak_length:
                 longest_peak_length = current_peak_length
+                peak_summit_index = current_summit_idx
             
             i = right_idx
         else:
             i += 1
 
-    return longest_peak_length
+    return longest_peak_length, peak_summit_index
